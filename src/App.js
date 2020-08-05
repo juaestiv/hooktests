d76171cdaa486f7644eb0c84632dc61b6d9f6776
juanan <juaestiv@gmail.com>
2020-08-05 20:02:02 +0200

    Prueba 1 OK

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//en mi explicacion esto es como si fuera una clase. Está fuera
+//de la función. Como un import. 
+// Luego en la función la instanciaremos
+const initialState = 0
+const reducer = (state, action) => {
+  switch (action) {
+    case 'increment': return state + 1;
+    case 'decrement': return state -1;
+    case 'reset': return 0;
+    default: throw new Error('Unexpected action');
+  }
+};
+
+
 function App() {
+  //como si instanciaramos el reducer
+
+  const [contador, dispatch] = useReducer(reducer,initialState);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +36,13 @@ function App() {
         >
           Learn React
         </a>
+        {/* Muestra el contador */}
+        {contador}
+        
+        {/* /ahora usamos la instancia */}
+        <button onClick={()=>dispatch('increment')}>+1</button>
+        <button onClick={()=>dispatch('decrement')}>-1</button>
+        <button onClick={()=>dispatch('reset')}>reset</button>
       </header>
     </div>
   );
